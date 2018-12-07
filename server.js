19e8e9983b432e8651d708e50ecbef0775f1bb56
David Gutierrez <dcgutierrez93@yahoo.com>
2018-12-07 09:17:40 -0600

    Setup Twilio SMS Messaging

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 'use strict';
 
 // Dependencies
+// ============================================
 const express = require('express');
 const bodyParser = require('body-parser');
+const twilio = require('twilio');
+
+var accountSid = 'AC3932e60c1d3ea3f63d65601c8cd831a5'; // Your Account SID from www.twilio.com/console
+var authToken = '6780e2cb6897f666d01324c4fb2bcef6';   // Your Auth Token from www.twilio.com/console
+
+// var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+
+client.messages.create({
+    body: 'Hello from Node',
+    to: '+19152035856',  // Text this number
+    from: '+19153018272' // From a valid Twilio number
+})
+.then((message) => console.log(message.sid));
 
 // Setup Express Server
 const app = express();
